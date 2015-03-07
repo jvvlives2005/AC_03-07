46a0ad235b9edc218198db517dc1c16516dd6c8c
jvvlives2005 <jvvlives2005@aol.com>
2015-03-07 15:29:02 -0500

    Polish Calculator

--- src/nyc/c4q/jvvlives2005/Main.java
+++ src/nyc/c4q/jvvlives2005/Main.java
@@ -1,8 +1,25 @@
 package nyc.c4q.jvvlives2005;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Quel age avez vous? ");
+        Scanner input = new Scanner(System.in);
+        int age = input.nextInt();
+        int ticketPrice;
+
+	   // aq is at the cinema
+	   if (age <= 13) {
+           ticketPrice = 8;
+           System.out.println("Vous payez: " + ticketPrice + " euros. ");
+       }
+        if (age <= 64) {
+           ticketPrice = 13;
+        } else {
+           ticketPrice = 10;
+        }
+        System.out.println("Vous payez: " + ticketPrice + " euros. ");
     }
 }
---
+++ src/polishCalculator.java
@@ -0,0 +1,47 @@
+/**
+ * Created by c4q-joshelynvivas on 3/7/15.
+ */
+import java.util.Scanner;
+
+public class polishCalculator {
+    public static void main(String[] args){
+        double firstNum;
+        double secondNum;
+        double answer;
+        String operator;
+
+        Scanner input = new Scanner (System.in);
+        System.out.println("Please enter your first number:");
+        firstNum = input.nextDouble();
+
+        System.out.println("Please enter your second number:");
+        secondNum = input.nextDouble();
+
+        System.out.println("Please enter an operation:");
+        operator = input.next();
+
+        if (operator.equals("+")){
+            answer = firstNum + secondNum;
+            System.out.println("Result: " + answer);
+        }
+
+        if (operator.equals("-")){
+            answer = firstNum - secondNum;
+            System.out.println("Result: " + answer);
+        }
+
+        if (operator.equals("*")){
+            answer = firstNum * secondNum;
+            System.out.println("Result: " + answer);
+        }
+        if (operator.equals("/")){
+            answer = firstNum / secondNum;
+            System.out.println("Result: " + answer);
+        }
+        if (operator.equals("%")){
+            answer = firstNum % secondNum;
+            System.out.println("Result: " + answer);
+        }
+
+    }
+}
